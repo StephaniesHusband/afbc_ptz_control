37159a56421b18941f22cc2004d1e72f1ad26b89
Scott Erwin <stephanieshusband@gmail.com>
2021-03-28 17:28:41 -0500

    updates

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,31 +1,6 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        afbc_ptz_control
-      </h1>
-      <h2 class="subtitle">
-        My striking Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+	<div class="container">
+	</div>
 </template>
 
 <script>
